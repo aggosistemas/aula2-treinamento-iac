69d6f0e92a57bfccd5c90300d65edb0f7926e462
aggosistemas <125746491+aggosistemas@users.noreply.github.com>
2025-09-09 18:57:52 -0300

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,3 @@
 output "html_bucket_url" {
-  value = "https://storage.googleapis.com/${google_storage_bucket.html_bucket.name}/index.html"
+  value = "https://storage.googleapis.com/${google_storage_bucket.html_bucket2.name}/index.html"
 }
